dc4dada70f2f228af03752d18a242194ac88805d
Alecia Hagen <alecia.h98@gmail.com>
2024-12-04 13:32:52 -0600

    I would be lying if I said I had enough energy to do this all now.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,10 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+myCollection = [];
 
+function addToCollection(collection, title, artist, yearPublished){
 
+}
 
 
 
